277e34b48b3cf9b7933dece01284c2741b2e38e7
Sitara Alayev <sitaraalayev@gmail.com>
2021-12-19 18:54:34 +0200

    Change view of screen.

---
+++ screens/View_folders.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Styles from '../Styles'
+
+const View_folders = (props) =>{
+
+    return(
+        <View style={Styles.container}>
+             <View style={Styles.SelectContainer}>
+             <Text>View_folders screen</Text>
+             </View>
+
+    </View>
+    );
+};
+
+export default View_folders;
