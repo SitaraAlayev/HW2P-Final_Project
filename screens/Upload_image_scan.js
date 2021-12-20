1bd31d87356cfad51b611c4976050f48d5bfa358
Sitara Alayev <sitaraalayev@gmail.com>
2021-12-20 10:32:55 +0200

    Added on press button.

--- screens/Upload_image_scan.js
+++ screens/Upload_image_scan.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { View,Text } from "react-native";
-import Styles from "../Styles";
-import  {Select_Box } from "../components/Select_Box";
+import Styles from "../Styles"
 import {Main_button} from "../components/Main_button";
 
 
-export const Upload_image_scan =() =>{
-    return(
+const Upload_image_scan =(props) =>{      
+      return(
         <View style={Styles.container}>
             <View style={Styles.SelectContainer}>
              <Text style={Styles.textstyle} >
                 ?האם תרצה לעלות תמונה מהגלריה או לצלם במצלמה
                 </Text>
-                <Main_button style={Styles.SelectContainer_buttons}>
+                <Main_button style={Styles.SelectContainer_buttons} onPress= {() =>{props.navigation.navigate('Image_picker_camera')} }>
                      מצלמה
                 </Main_button>
-                <Main_button style={Styles.SelectContainer_buttons}>
+                <Main_button style={Styles.SelectContainer_buttons} onPress= {() =>{props.navigation.navigate('Image_picker_gallery')} }> 
                      גלריה
                 </Main_button>
                 </View>
@@ -25,4 +24,4 @@ export const Upload_image_scan =() =>{
     );
 };
 
-export default Upload_image_scan
+export default Upload_image_scan;
