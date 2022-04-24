2e04bdd1056638cdfce7eb2a4c947ca001d37d40
Sitara Alayev <sitaraalayev@gmail.com>
2022-04-24 21:40:23 +0300

    added header button for adding new folder in view folder screen.

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -1,18 +1,16 @@
-import react from 'react';
-import { View, Button, Image, Text, StyleSheet, Alert } from 'react-native';
+import React from 'react';
 import {HeaderButton} from 'react-navigation-header-buttons';
 import {Ionicons} from '@expo/vector-icons';
 import { Platform } from 'react-native';
 
-const HeaderButton = props =>{
+const CustomHeaderButton = props =>{
     return (
-    <HeaderButton
+    <HeaderButton 
      {...props} 
       IconComponent={Ionicons}
       iconSize={23} 
-      color={ Platform.OS ==='android' ? '#AD67EA' : '#AD67EA' }
-
-    />
+      color={ Platform.OS ==='android' ? 'white' : '#AD67EA' }
+      />
     );
 };
-export default HeaderButton;
+export default CustomHeaderButton;
