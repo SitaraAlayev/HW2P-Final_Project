e21bda0e25dea77bea70c9302b5801e42d112472
Sitara Alayev <sitaraalayev@gmail.com>
2022-04-24 21:42:52 +0300

    change style.

--- screens/Home_page.js
+++ screens/Home_page.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import { View, Image,Text } from 'react-native';
+import { View, Image } from 'react-native';
 import {Main_button} from '../components/Main_button'
 import Styles from '../Styles'
 
 
 const Home_page = (props) =>
  {
+   
   return (
-      <View style={Styles.container}>
-          <Image 
-          source={require('../assets/logo_final.png')} 
-          style={Styles.image} 
-          resizeMode="contain"/>
-          <Main_button onPress= {() =>{props.navigation.navigate('View_folders')}}>
-                     צפייה בתיקיות
-              </Main_button>
-            <Main_button onPress= {() =>{props.navigation.navigate('Upload_image_scan')}} >
-                  העלה תמונה לסריקה   
-              </Main_button>
-              
-      </View>
+    <View style={Styles.container}>
+        <Image 
+        source={require('../assets/logo_final.png')} 
+        style={Styles.image} 
+        resizeMode="contain"/>
+        <View>
+        <Main_button onPress= {() =>{props.navigation.navigate('View_folders')}}>
+                צפייה בתיקיות
+            </Main_button>
+          <Main_button onPress= {() =>{props.navigation.navigate('Upload_image_scan')}} >
+                העלה תמונה לסריקה   
+            </Main_button>
+            </View>
+            
+    </View>
 
-  );
+);
 };
+
+
 Home_page.navigationOptions={
-  headerTitle:'HW2P SCANNER',
-  headerStyle:{backgroundColor:'#AD67EA'},
-  headerTintColor:'white'
+  headerTitle:'HW2P SCANNER'
 };
 
 export default Home_page;
