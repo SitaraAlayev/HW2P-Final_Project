748278d7a3bd822ddb457021a25111a6d58d1b28
Sitara Alayev <sitaraalayev@gmail.com>
2022-04-24 21:36:38 +0300

    View folder screen.

--- screens/View_folders.js
+++ screens/View_folders.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React,{useState} from 'react';
+import { View, Text,Platform,ScrollView, FlatList } from 'react-native';
 import Styles from '../Styles'
+import { HeaderButtons,Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
+import Add_Folder from '../components/Add_Folder';
 
-const View_folders = (props) =>{
-
+const View_folders = props =>{
     return(
-        <View style={Styles.container}>
-             <View style={Styles.SelectContainer}>
-             <Text>View_folders screen</Text>
-             </View>
-
-    </View>
+            <View style={Styles.container}>
+                
+                <Text>מסך צפייה בתיקיות </Text>
+            </View>
     );
-};
+}
+  
+View_folders.navigationOptions= navDate =>{
+return {
+    headerTitle:'צפייה בתיקיות',
+    headerRight: (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='Add' iconName={Platform.OS ==='android' ? 'md-add' :'ios-add'}
+        onPress ={() =>{}}/>
+    </HeaderButtons>)
 
+
+};
+}
 export default View_folders;
